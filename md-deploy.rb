03286ef6e1f76e5eb4ada8bf8a4d395bba24df4d
samuelsachin <samuelsachin@hotmail.co.uk>
2014-07-15 11:14:15 +0100

    done

--- md-deploy.rb
+++ md-deploy.rb
@@ -2,4 +2,6 @@
 presidents = ["Ford", "Carter", "Reagan", "Bush1", "Clinton", "Bush2"]
 for ss in 0...presidents.length
 	print ss, ": ", presidents[ss], "\n";
+
+	this is wriiten from the laptop sachin
 end
